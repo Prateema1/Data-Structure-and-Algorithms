0c675fedc1e41bf4272fb68f173160e79896c15a
Prateema1 <prateema.adhikari@gmail.com>
2023-01-27 20:42:34 +0530

    Added fibonacci program using recursion

---
+++ src/com/pratima/Fibo.java
@@ -0,0 +1,19 @@
+package com.pratima;
+
+public class Fibo {
+    public static void main(String[] args) {
+        int ans = fibo(4);
+        System.out.println(ans);
+    }
+
+    static int fibo(int n) {
+        //Base condition
+        if(n < 2) {
+            return n;
+        }
+        return fibo(n-1) + fibo(n-2);
+    }
+}
+
+
+
---
+++ src/com/pratima/Main.java
@@ -0,0 +1,15 @@
+package com.pratima;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        do while syntax
+        do {
+
+        } while(condition);
+
+        In do while, the do while loop is going to execute at least once
+         */
+    }
+}
